b0bbc9f5b16dc9aab661f8b1ea06b4a639fc1fc2
diplamtuan <85506794+diplamtuan@users.noreply.github.com>
2023-03-22 18:37:28 +0700

    Update login

--- DB/dbconnect.php
+++ DB/dbconnect.php
@@ -8,7 +8,6 @@ $dbname = "doanweb2";
 // Create connection
 $conn = mysqli_connect($servername, $username, $passwd, $dbname);
 
-
 // Check connection
 if ($conn->connect_error) {
     die("connection failed: " . $conn->connect_error);
---
+++ view/custommerView.php
@@ -0,0 +1,16 @@
+<?php
+class CustommerView extends Custommer
+{
+    public function showCustommer()
+    {
+        $result = $this->getCustommers('diplamtuan');
+        foreach ($result as $value) {
+            echo $value["tenkhachhang"];
+            echo "<br>";
+            echo $value["diachi"];
+            echo "<br>";
+            echo $value["sdt"];
+            echo "<br>";
+        }
+    }
+}

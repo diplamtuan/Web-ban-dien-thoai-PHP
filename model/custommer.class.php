79631f042df169cb2adb01ef0f8f588e45a62ab9
diplamtuan <85506794+diplamtuan@users.noreply.github.com>
2023-04-06 16:50:19 +0700

    Update profile user

--- model/custommer.class.php
+++ model/custommer.class.php
@@ -1,27 +1,30 @@
 <?php
+include '../DTO/CustommerModel.php';
 class Custommer extends Db
 {
-    protected function getCustommers()
+    protected function getCustommerById($id)
     {
-        $sql = "SELECT * FROM khachhang";
+        $sql = "SELECT * FROM khachhang where id_khachhang='$id'";
         $result = mysqli_query($this->connect(), $sql);
-        $resultCheck = mysqli_num_rows($result);
-        $data = array();
-        if ($resultCheck > 0) {
-            while ($row = mysqli_fetch_assoc($result)) {
-                $data[] = $row;
-            }
-            return $data;
-        }
+        if (mysqli_num_rows($result) > 0) {
+            $row = mysqli_fetch_array($result);
+            return $row;
+        } else return false;
     }
 
-    protected function insertCustommer($tenkhachhang, $diachi, $sdt, $email, $tentaikhoan, $mathau, $trangthai)
+    protected function updateCustommerProfile(CustommerModel $custommer)
     {
-        $sql = "INSERT into khachhang (tenkhachhang,diachi,sdt,email,tentaikhoan,mathau,trangthai) VALUES('$tenkhachhang','$diachi','$sdt','$email','$tentaikhoan',' $mathau','$trangthai')";
+        $id_kh = $custommer->getId();
+        $tenkh = $custommer->getTenKH();
+        $gioitinh = $custommer->getGioitinh();
+        $diachi = $custommer->getDiachi();
+        $sdt = $custommer->getSdt();
+        $email = $custommer->getEmail();
+        $sql = "UPDATE khachhang 
+        set tenkhachhang = '$tenkh',gioitinh='$gioitinh',diachi='$diachi',sdt='$sdt',email='$email'
+        where khachhang.id_khachhang = '$id_kh'";
         if (mysqli_query($this->connect(), $sql)) {
-            echo "Success";
-        } else {
-            echo "Error" . $this->connect()->error;
-        }
+            return true;
+        } else return false;
     }
 }
--- view/custommerView.php
+++ view/custommerView.php
@@ -1,16 +1,8 @@
 <?php
 class CustommerView extends Custommer
 {
-    public function showCustommer()
+    public function getCustommerByIdView($id)
     {
-        $result = $this->getCustommers();
-        foreach ($result as $value) {
-            echo $value["tenkhachhang"];
-            echo "<br>";
-            echo $value["diachi"];
-            echo "<br>";
-            echo $value["sdt"];
-            echo "<br>";
-        }
+        return $this->getCustommerById($id);
     }
 }
